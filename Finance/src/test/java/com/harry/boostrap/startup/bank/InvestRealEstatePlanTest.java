a47f414990af1bf1bda7541c258c98c85c78f27d
harry <huangxh@dragonpass.com.cn>
2024-02-20 10:07:11 +0800

    优化提示

---
+++ Finance/src/test/java/com/harry/boostrap/startup/bank/InvestRealEstatePlanTest.java
@@ -0,0 +1,52 @@
+package com.harry.boostrap.startup.bank;
+
+import java.util.Date;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * @Description: 房地产投资计划书
+ * @Author: harry
+ * @CreateTime: 2024/2/20
+ */
+public class InvestRealEstatePlanTest {
+
+    @Test
+    public void test(){
+        //初始投入金额
+        float input=30000;
+        //月租金收入
+        long rent=1200;
+        //贷款总额
+        long principal=160000;
+        //贷款年利率
+        float interest=0.052f;
+        //贷款期数
+        int numOfPeriods=360;
+        String startDate=DateUtils.formatDateToStr(new Date());
+        List<RepaymentPlan> planList = EqualAmoutInterest.getAllPlans("建设银行",
+            LoanType.EQUAL_AMOUT_PRINCIPAL_AND_INTEREST, principal, interest, InterestType.Y, numOfPeriods,
+            startDate, null);
+        calculateInvestmentReturnRate(input,rent,planList.get(0));
+        print(planList);
+    }
+
+    private void calculateInvestmentReturnRate(float input, long rent, RepaymentPlan repaymentPlan) {
+        long npv= (long) (rent-repaymentPlan.getTotal());
+        float rate=npv*12/input;
+        System.out.println("初始投入资金："+input+" 元");
+        System.out.println("贷款总额："+repaymentPlan.getLoanInfo().getPrincipal()+"元,月供："+repaymentPlan.getTotal()+"元");
+        System.out.println("净现值："+npv+"元");
+        System.out.println("投资回报率："+rate*100+"%, "+Math.round(1/rate)+" 年内回本");
+    }
+
+    private static void print(List<RepaymentPlan> plans) {
+        float total=0;
+        float all=0;
+        for (RepaymentPlan plan:plans){
+            total+=plan.getInterest();
+            all+=plan.getTotal();
+        }
+        System.out.println("总利息是："+total+",总还款："+all);
+    }
+}
