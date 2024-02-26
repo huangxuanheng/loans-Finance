e20e551bf03ddef69c8af31e71a36f4b585d9052
harry <huangxh@dragonpass.com.cn>
2024-02-26 09:31:03 +0800

    后台启动脚本

--- Finance/startup.bat
+++ Finance/startup.bat
@@ -1,4 +1,4 @@
+@echo off
 set JAVA_HOME=D:\Program Files\Java\jdk1.8.0_341
 set PATH=%JAVA_HOME%\bin
-java -version
-java -jar target/startup-0.0.1-SNAPSHOT.jar
+start javaw -jar target/startup-0.0.1-SNAPSHOT.jar
